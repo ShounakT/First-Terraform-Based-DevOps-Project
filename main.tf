43f16034bee7de1a474e6880ed55802d01fef7c2
Shounak Todankar <52619386+ShounakT@users.noreply.github.com>
2025-11-01 13:59:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,48 +1,69 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.74.2"
+terraform{
+    required_providers{
+        aws = {
+
+            source = "hashicorp/aws"
+            version = "~>4.16"
+
+        }
+
     }
-  }
+    required_version = ">=1.2.0"
+    backend "s3"{
 
-  required_version = ">= 1.2.0"
+        bucket = "mytf3shounaktodankar"
+        dynamodb_table = "myDevOpsDynamoDBShounakTodankar"
+        key = "myFiles/myTFStateFile/terraform.tfstate"
+        region = "ap-south-1"
+        encrypt = true
 
-  backend "s3" {
-        bucket         = "mytf3shounaktodankar"
-        key            = "ShouDevOps/write_your_first_terraform_project/aws/local_state/terraform.tfstate"
-        region         = "ap-south-1"
-        dynamodb_table = "MyFirstDynamoDBShounakT"
-        encrypt        = true
-  }
-
+    }
 }
 
 provider "aws" {
-  region  = "ap-south-1"
-  access_key = "your_access_key"
-  secret_key = "your_secret_key"
+
+    region = "ap-south-1"
+
 }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami_id"
+
+resource "aws_instance" "myDevOpsEC2" {
+  ami           = "ami-02d26659fd82cf299"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "MyDevOpsServer2"
+    Name = "myDevOpsEC2"
   }
 }
 
 resource "aws_s3_bucket" "my_unique_bucket" {
   bucket = "mytf3shounaktodankar"
   tags = {
-    Name        = "MyFirstS3BucketShounakTodankar"
+    Name        = "myDevOpsS3BucketShounakTodankar"
   }
 }
 
-resource "aws_dynamodb_table" "terraform_lock" {
-  name         = "MyFirstDynamoDBShounakT"
-  billing_mode = "PAY_PER_REQUEST"
+resource "aws_s3_bucket_versioning" "versioning_example" {
+  bucket = "mytf3shounaktodankar"
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
+  bucket = "mytf3shounaktodankar"
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "AES256"
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "example" {
+  name             = "myDevOpsDynamoDBShounakTodankar"
+  billing_mode     = "PAY_PER_REQUEST"
   hash_key     = "LockID"
 
   attribute {
